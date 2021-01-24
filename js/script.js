6809d826c4e81ff80c17b396a68d2ef21942fda8
Michele Cofano <brutalmachinelol@gmail.com>
2021-01-24 23:09:17 +0100

    Creazione carte con JS

--- js/script.js
+++ js/script.js
@@ -33,6 +33,7 @@ const editions = {
 
 const cards = [{
 
+// prima carta --------------------------------
   cardName: 'Grizzly Bears',
 
   cost: {
@@ -57,34 +58,155 @@ const cards = [{
     toughness: 2
   }
 
+},
+
+
+// seconda carta ---------------------------------
+{
+
+  cardName: 'Sviluppatore guerriero',
+
+  cost: {
+    genericCostNumber: 3,
+    costFields: [ // colors array con riferimento a fieldCodes
+      fieldCodes[2],
+      fieldCodes[3]
+    ],
   },
-  {
 
-    cardName: 'Sviluppatore guerriero',
+  picture: 'images/g.png',  // da inserire immagine
+  cardType: cardTypes[1],
+  cardObject: 'Bear',
 
-    cost: {
-      genericCostNumber: 3,
-      costFields: [ // colors array con riferimento a fieldCodes
-        fieldCodes[2],
-        fieldCodes[3]
-      ],
-    },
+  editionType: editions['BL'],
 
-    picture: 'images/g.png',  // da inserire immagine
-    cardType: cardTypes[1],
-    cardObject: 'Bear',
+  description: 'Lo sviluppatore guerriero spezza i byte in bit!',
+  story: 'Lo sviluppatore guerriero è una forma di essere umano evoluto.',
 
-    editionType: editions['BL'],
+  score: {
+    power: 5,  // r
+    toughness: 3
+  }
 
-    description: 'Lo sviluppatore guerriero spezza i byte in bit!',
-    story: 'Lo sviluppatore guerriero è una forma di essere umano evoluto.',
+},
 
-    score: {
-      power: 5,  // r
-      toughness: 3
-    }
 
-    },
-]
+// terza carta -----------------------------------
+{
 
+  cardName: 'Mewtwo',
+
+  cost: {
+    genericCostNumber: 6,
+    costFields: [ // colors array con riferimento a fieldCodes
+      fieldCodes[0],
+      fieldCodes[0],
+      fieldCodes[0]
+    ],
+  },
+
+  picture: 'images/mewtwo.png',  // da inserire immagine
+  cardType: cardTypes[1],
+  cardObject: 'Pokemon',
+
+  editionType: editions['BL'],
+
+  description: 'Il potere della mente più forte del mondo',
+  story: 'Il pokemon clonato dalla leggenda di Mew',
+
+  score: {
+    power: 5,  // r
+    toughness: 5
+  }
+
+},
+
+// quarta carta ------------------------------------
+{
+
+  cardName: 'Kelenvorita mascherato',
+
+  cost: {
+    genericCostNumber: 2,
+    costFields: [ // colors array con riferimento a fieldCodes
+      fieldCodes[0],
+      fieldCodes[2]
+    ],
+  },
+
+  picture: 'images/kelenvorita.png',  // da inserire immagine
+  cardType: cardTypes[1],
+  cardObject: 'Mezz\'orco',
+
+  editionType: editions['SP'],
+
+  description: 'quando arriva a 0 punti ferita acquista un punto ferita invece di finire al cimitero e recupera forza',
+  story: 'Kelenvor predica la grigia ed imparziale giustizia della morte',
+
+  score: {
+    power: 1,  // r
+    toughness: 4
+  }
+
+},
+
+
+// quinta carta ------------------------------------
+{
+
+  cardName: 'carta trappola',
+
+  cost: {
+    genericCostNumber: 1,
+    costFields: [ // colors array con riferimento a fieldCodes
+      fieldCodes[1],
+      fieldCodes[2]
+    ],
+  },
+
+  picture: 'images/cartatrappola.png',  // da inserire immagine
+  cardType: cardTypes[2],
+  cardObject: 'Carta trappola',
+
+  editionType: editions['SP'],
+
+  description: 'Quando il nemico attacca, si attiva questa carta trappola, riducendo il danno inflitto del 90%',
+  story: 'Questa carta è stata creata con lo scopo di far ragequittare il nemico',
+
+  score: {
+    power: 0,  // r
+    toughness: 1
+  }
+
+},
+
+// sesta carta -------------------------------------
+{
+
+  cardName: 'Cristallo della libertà',
+
+  cost: {
+    genericCostNumber: 3,
+    costFields: [ // colors array con riferimento a fieldCodes
+      fieldCodes[2],
+      fieldCodes[0]
+    ],
+  },
+
+  picture: 'images/cristallo.png',  // da inserire immagine
+  cardType: cardTypes[3],
+  cardObject: 'Cristallo',
+
+  editionType: editions['SP'],
+
+  description: 'Il cristallo della libertà permette, all\'uso, di scongelare i tuoi alleati e di infliggere danni ai nemici ',
+  story: 'Cristallo creato con le lacrime degli dei',
+
+  score: {
+    power: 0,  // r
+    toughness: 0
+  }
+}
+
+];
 console.log(cards);
